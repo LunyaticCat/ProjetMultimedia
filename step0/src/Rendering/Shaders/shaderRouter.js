3578f73bf1051c5379be856989415ed24a32c252
Gatien <depeyre.gatien@gmail.com>
2023-12-11 16:49:04 +0100

    complete shaderRouter

--- step0/src/Rendering/Shaders/shaderRouter.js
+++ step0/src/Rendering/Shaders/shaderRouter.js
@@ -22,9 +22,27 @@ function setupShaders(gl){
 
     //TODO : Call the .glsl files
 
-    //TODO : 
+    //-----
 
+    //TODO : Link vertexShader and fragmentShader to the shader sources
     var vertexShader = compileShader(vertexShaderSource, gl.VERTEX_SHADER);
     var fragmentShader = compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
+    //------
 
+    // create and attach the shaders with gl
+    program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    // set attributes for shaders
+    positionLocation = gl.getAttribLocation(program, "a_position");
+    colorLocation = gl.getUniformLocation(program, "u_color");
+    
+    matrixLocation = gl.getUniformLocation(program, "u_matrix");
+
+    color = [Math.random(), Math.random(), Math.random(), 1];
+
+    positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 }
